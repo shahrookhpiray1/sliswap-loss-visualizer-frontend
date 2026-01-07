c6294ebf42f52560c536ab4d1887a7d5c46f14cf
shahrookhpiray1 <mohammadreza.piray1377@gmail.com>
2026-01-07 17:18:29 +0330

    deploy frontend to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-// client/vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/sliswap-loss-visualizer-frontend/',
+  build: {
+    outDir: 'docs'
+  },
   plugins: [react()],
 });
