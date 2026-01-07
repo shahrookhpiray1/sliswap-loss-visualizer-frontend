f0b51067f27bafdfd72f44006778fe3700167399
shahrookhpiray1 <mohammadreza.piray1377@gmail.com>
2026-01-07 15:34:06 +0330

    deploy

--- src/services/endlessClient.ts
+++
@@ -1,5 +0,0 @@
-// src/services/endlessClient.ts
-import { Endless, EndlessConfig, Network } from '@endlesslab/endless-ts-sdk';
-
-const config = new EndlessConfig({ network: Network.MAINNET });
-export const endlessClient = new Endless(config);
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-// client/vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: './',
-  plugins: [react()],
-});
